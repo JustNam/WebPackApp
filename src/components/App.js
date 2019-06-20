59c52445e3f028bd02e83cdab3dc75d44bf05341
Got It <justn4m@gmail.com>
2019-06-20 13:48:36 +0700

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
 import React, {Component} from 'react';
+import { Route } from 'react-router-dom'
+import {Link} from 'react-router-dom'
+
+import Loadable from 'react-loadable';
+
+const LoadableDiv = Loadable({
+    loader: () => import('./Content'),
+    loading() {
+        return <div>Loading...</div>
+    }
+})
 
 class App extends Component {
     render() {
         return  <div>
             <h1> My React app</h1>
+            <Link className="addIcon" to="/Add">  </Link>
+            <Route path='/Add' render={({ history }) => (
+                <LoadableDiv onHistory={history} />
+            )} />
         </div>;
     }
 }
